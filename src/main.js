a6ae825112ed086bb3ffc119245491a9bd3fec81
Braedan Royer <royerbraedan@gmail.com>
2025-11-18 15:44:07 -0800

    finished some styling, added a message-el div and id to the paragraph element

--- src/main.js
+++ src/main.js
@@ -4,13 +4,21 @@ let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = ``;
+const startGameBtn = document.querySelector("#start-game-btn");
+const messageEl = document.querySelector('#message-el')
 
-if (sum < 21) {
-  message = `Still in the game.`;
-} else if (sum === 21) {
-  hasBlackjack = true;
-  message = `You have blackjack!`;
-} else {
-  message = `You're out of the game`;
-  isAlive = false;
+function startGame() {
+  if (sum < 21) {
+    message = `Still in the game.`;
+  } else if (sum === 21) {
+    hasBlackjack = true;
+    message = `You have blackjack!`;
+  } else {
+    message = `You're out of the game`;
+    isAlive = false;
+  }
 }
+
+startGameBtn.addEventListener("click", startGame);
+
+console.log(messageEl)
