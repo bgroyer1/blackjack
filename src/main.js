9e4727bcb0f5d7444a683a0f469829bb73f978b4
Braedan Royer <royerbraedan@gmail.com>
2025-11-18 16:09:25 -0800

    added a new game button, added a favicon, created new game function, connected it to the html element. function is currently not doing anything.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,39 @@
-let firstCard = 8;
+let firstCard = 10;
 let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = ``;
 const startGameBtn = document.querySelector("#start-game-btn");
-const messageEl = document.querySelector('#message-el')
+const messageEl = document.querySelector('.message-el')
+const sumEl = document.querySelector('#sum-el')
+const cardsEl = document.querySelector('#cards-el')
+const newGameBtn = document.querySelector('#new-game-btn')
 
 function startGame() {
+  cardsEl.innerHTML = `Cards: <span class='primary-text-color'>${firstCard} ${secondCard}</span>`
+  sumEl.innerHTML = `Sum: <span class='primary-text-color'>${sum}</span>`;
   if (sum < 21) {
-    message = `Still in the game.`;
+    message = renderMessageHTML(`Still in the`, 'game.')
   } else if (sum === 21) {
     hasBlackjack = true;
-    message = `You have blackjack!`;
+    message = renderMessageHTML('You have', 'blackjack!')
   } else {
-    message = `You're out of the game`;
+    message = renderMessageHTML("You're out of the", 'game.')
     isAlive = false;
   }
+  messageEl.innerHTML = message
 }
+
+function renderMessageHTML(secondary, primary) {
+  return `<p class='message-el'>${secondary} <span class='primary-text-color bold'>${primary}</span></p>`
+}
+
+function newGame() {
+  console.log('clicked')
+}
+
+newGameBtn.addEventListener('click', newGame)
 
 startGameBtn.addEventListener("click", startGame);
 
-console.log(messageEl)
